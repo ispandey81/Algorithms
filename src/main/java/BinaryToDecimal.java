b1b8eadbdb05e994b696a117195bd6bcd63e89d0
indra <ispandey81@gmail.com>
2021-09-11 16:35:37 +0800

    Initial commit

---
+++ src/main/java/BinaryToDecimal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String binaryString = scanner.nextLine();
+        System.out.println(convertBinaryToDecimal(binaryString));
+    }
+
+    private static int convertBinaryToDecimal(String binaryString) {
+        StringBuilder stringBuilder = new StringBuilder(binaryString);
+        String reversedString = stringBuilder.reverse().toString();
+        int sum = 0;
+        for (int i=0; i< reversedString.length(); i++) {
+            sum += Character.getNumericValue(reversedString.charAt(i)) * Math.pow(2, i);
+        }
+        return sum;
+    }
+}
